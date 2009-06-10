86cf37e81ecbf5eb5c0ea4671bb406774c2da76d
Stig <Stig@McFeasty.local>
2009-06-10 08:33:25 +0200

    Added JZDomain and ClientDomain classes

---
+++ grails-app/domain/JZDomain.groovy
@@ -0,0 +1,13 @@
+class Person {
+	String fullName
+	String email
+	String company
+	//Int telephone (restricted)
+	Boolean isHero = false
+	Boolean alumni = false
+	List<RfidCard> rfidCards
+}
+
+class RfidCard {
+        String rfidId
+}
