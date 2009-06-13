e6a4a303aa6c8665f7a98ff6844364de211a2cc5
Stig <Stig@McFeasty.local>
2009-06-13 13:36:05 +0200

    moved Location

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,7 @@
 class BootStrap {
 
   def init = {servletContext ->
+    def locationForRfid1 = new Location(xyLocation:"Here", cleartext:"not so clear").save()
     def rfidcard1 = new RfidCard(rfidId:"id one").save()
     def rogerMoore = new Person(fullName: "Roger Moore", email: "roger@moore.com", company: "roger more", isHero: false, alumni: false, rfidCards: [rfidcard1]).save()
   }
