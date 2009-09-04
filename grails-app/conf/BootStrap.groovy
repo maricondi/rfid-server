5bd30f3c0a90c02b51e5916494bfb8a70cd4a9a4
beiske <beiske@l31w55z.bouvet.no>
2009-09-04 16:11:25 +0200

    As far as I got on adding the acegi-plugin

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,14 +1,26 @@
 import clientdomain.*
 import jzdomain.*
 
+//import org.grails.plugins.springsecurity.service.AuthenticateService 
+
 class BootStrap {
+  def authenticateService
 
   def init = {servletContext ->
+	//Create the security roles
+	def adminRole = new Role(authority: "ROLE_ADMIN", description: "Administrator access").save()
+	def userRole = new Role(authority: "ROLE_USER", description: "Ordinary authenticated user").save()
+	def password = authenticateService.passwordEncoder("changeMe")
+	
+	def adminUser = new User(username:"admin",userRealName:"Administrator",passwd:password,enabled:true,emailShow:true,description:"admin account",email:"nomail@nomail.no").save()
+	adminRole.addToPeople(adminUser)
+	adminRole.save()
+	
     //Create a test dummy set of data
 
     //Insertion of RFID and person data by JavaZone
-    Person person1 = new Person(fullName: "Roger Moore", email: "roger@moore.com", company: "roger more", isHero: false, alumni: false).save()
-    Person person2 = new Person(fullName: "Daniel Craig", email: "daniel@imdb.com", company: "Daniels cakeshop", isHero: true, alumni: false).save()
+    Person person1 = new Person(fullName: "Roger Moore", email: "roger@moore.com", company: "roger more", isHero: false, alumni: false, telephone:"+47 22 22 22 22").save()
+    Person person2 = new Person(fullName: "Daniel Craig", email: "daniel@imdb.com", company: "Daniels cakeshop", isHero: true, alumni: false, telephone:"").save()
 
     RfidCard rfidcard1 = new RfidCard(rfidId:"id one", ownedBy:person1).save()
     RfidCard rfidcard2 = new RfidCard(rfidId:"135aa1fd", ownedBy:person2).save()
---
+++ grails-app/conf/SecurityConfig.groovy
@@ -0,0 +1,14 @@
+security {
+
+	// see DefaultSecurityConfig.groovy for all settable/overridable properties
+
+	active = true
+
+	loginUserDomainClass = "User"
+	authorityDomainClass = "Role"
+	
+	useRequestMapDomainClass = false
+	useControllerAnnotations = true
+	loginFormUrl = '/html/login/auth'
+	basicProcessingFilter = true
+}
